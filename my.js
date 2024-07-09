ebe307a8916dfa4bd0c7a2c1cfc98b15b017685e
Kavitha Sampath <kavitha_bcomca@yahoo.co.in>
2024-07-09 16:40:03 +0100

    First Commit

---
+++ my.js
@@ -0,0 +1,11 @@
+let change = document.getElementById("kavi");
+change.style.fontFamily= "TimesNewRoman";
+
+let element = document.querySelector('button');
+
+function completed(){
+element.innerHTML="Submitted"
+
+
+}
+element.onclick = completed
